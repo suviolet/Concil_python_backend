a1c464bbc07a35965bdd5fe367cdb3eef3129fcb
Suellen <suellendesalles@gmail.com>
2019-03-29 16:24:16 -0300

    Creates Animal model

--- doghouse/models.py
+++ doghouse/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+
+    specie_choice = (('cat', 'gato'), ('dog', 'cachorro'))
+    gender_choice = (('female', 'fêmea'), ('male', 'macho'))
+    age_choice = (
+        ('kitten', 'filhote'), ('young', 'jovem'),
+        ('adult', 'adulto'), ('senior', 'idoso'),
+    )
+    size_choice = (
+        ('small', 'pequeno'), ('medium', 'médio'), ('large', 'grande')
+    )
+    hair_choice = (('short', 'curto'), ('medium', 'médio'), ('long', 'longo'))
+
+    name = models.CharField(max_length=20)
+    specie = models.CharField(max_length=10, choices=specie_choice)
+    gender = models.CharField(max_length=10, choices=gender_choice)
+    age = models.CharField(max_length=10, choices=age_choice)
+    size = models.CharField(max_length=10, choices=size_choice)
+    hair = models.CharField(max_length=10, choices=hair_choice)
+    color = models.CharField(max_length=20)
+    description_ = models.TextField()
+    address = models.CharField(max_length=25)
+
